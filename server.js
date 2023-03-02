566024ba2ea02290fdc2cf78a24528d20d496f53
Athos Georgiou <athosg82@gmail.com>
2023-03-02 00:04:34 +0000

    extract key to .env file

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const multer = require('multer');
-const Clarifai = require('clarifai');
-const path = require('path');
+const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const Clarifai = require("clarifai");
+const path = require("path");
 
 const app = express();
 const port = 3000;
+require("dotenv").config();
 
 // Set up the Clarifai API client with your API key
 const clarifai = new Clarifai.App({
-  apiKey: ''
+  apiKey: process.env.API_KEY,
 });
 
 // Use bodyParser middleware to parse request bodies as JSON
@@ -24,22 +25,23 @@ const upload = multer({
 });
 
 // Define a route for the home page
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
 });
 
 // Define a route for the image recognition page
-app.get('/predict', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'predict.html'));
+app.get("/predict", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "predict.html"));
 });
 
 // Define a route to handle predictions
-app.post('/predict', upload.single('file'), (req, res) => {
+app.post("/predict", upload.single("file"), (req, res) => {
   const { buffer, mimetype } = req.file;
 
   // Use the Clarifai API client to predict image contents
-  clarifai.models.predict(Clarifai.GENERAL_MODEL, { base64: buffer.toString('base64') })
-    .then(response => {
+  clarifai.models
+    .predict(Clarifai.GENERAL_MODEL, { base64: buffer.toString("base64") })
+    .then((response) => {
       const predictions = response.outputs[0].data.concepts;
       res.send(`
         <html>
@@ -50,16 +52,21 @@ app.post('/predict', upload.single('file'), (req, res) => {
             <h1>Image Recognition Results</h1>
             <p>The following objects were detected in the image:</p>
             <ul>
-              ${predictions.map(prediction => `<li>${prediction.name} (${prediction.value})</li>`).join('')}
+              ${predictions
+                .map(
+                  (prediction) =>
+                    `<li>${prediction.name} (${prediction.value})</li>`
+                )
+                .join("")}
             </ul>
             <p><a href="/">Back to Home</a></p>
           </body>
         </html>
       `);
     })
-    .catch(err => {
+    .catch((err) => {
       console.error(err);
-      res.status(500).send('Error predicting image contents.');
+      res.status(500).send("Error predicting image contents.");
     });
 });
 
